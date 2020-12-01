9cc14c3806440eb00e3cc2fe9adc070885956ada
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-12-01 15:15:13 +0000

    Added headings and tests for printer class

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,7 +1,14 @@
 class Printer
 
   def print(statement)
-    statement
+    print_heading
+    statement.each { |transaction| puts transaction.date + " || " + transaction.amount.to_s + " || " + transaction.balance.to_s}
   end 
+
+  private
+
+  def print_heading
+    puts "date || credit || debit || balance"
+  end
 
 end 
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,13 +2,20 @@ require './lib/printer'
 
 describe Printer do
   subject(:printer) {described_class.new}
-  let(:statement) {double(:statement)}
+  let(:transaction1) { double(:transaction, date: "10/01/2012", amount: "1000", balance: "1000") }
 
   it 'respondes to print' do
     expect(printer).to respond_to(:print)
   end
+ 
+  describe '#print' do
 
-  it 'prints a statement' do
-    expect(printer.print(statement)).to eq(statement)
-  end
+    it 'should print the column' do
+      expected_heading = "date || credit || debit || balance\n"
+      expected_transaction = "10/01/2012 || 1000 || 1000\n"
+      statement = [transaction1]
+      expected_statement = expected_heading + expected_transaction
+      expect { printer.print(statement) }.to output(expected_statement).to_stdout
+    end
+  end 
 end
